b5360cd5b77b6d2ac4fe2b8b9f36176900b6ed8c
kpowkitty <witchikittikat@gmail.com>
2026-04-25 13:22:33 -0700

    routes: message POST for game chat

--- src/routes/api/messages.ts
+++ src/routes/api/messages.ts
@@ -11,6 +11,7 @@ import { requireAuth } from "../middleware.js";
 import MessageRepository from "../../models/message.js";
 import { GameParams } from "../../types.js";
 import logger from "../../util/logger.js";
+import { RequestError } from "../../util/error.ts";
 
 const router = Router();
 
@@ -34,12 +35,45 @@ router.get(
 );
 
 /**
- * Send game chat message.
- * xxx
+ * Send a game chat message.
  */
-router.post("/games/:id/messages", requireAuth, (_req: Request<GameParams>, res: Response) => {
-  res.json(null);
-});
+router.post(
+  "/games/:id/messages",
+  requireAuth,
+  async (
+    req: Request<GameParams, unknown, { body: string }>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const userFrom = req.session.userId;
+      if (!userFrom) {
+        throw new RequestError("Unable to complete request: missing user_from", 400);
+      }
+
+      const msgBody = req.body.body;
+      if (!msgBody) {
+        throw new RequestError("Unable to complete request: missing body", 400);
+      }
+
+      const gameId = req.params.id;
+      if (!gameId) {
+        throw new RequestError("Unable to complete request: missing game_id", 400);
+      }
+
+      const msg = await MessageRepository.create({
+        user_from: userFrom,
+        body: msgBody,
+        game_id: gameId,
+      });
+
+      res.json(msg);
+    } catch (err) {
+      logger.error(String(err));
+      next(err);
+    }
+  },
+);
 
 /**
  * Get DM inbox.
